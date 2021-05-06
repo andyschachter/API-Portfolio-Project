0a997297ac66187a81ebd326b901bc95754177d1
Andy Schachter <andy.schachter@gmail.com>
2021-05-06 18:04:25 -0400

    added additional dummy brewery

--- breweries.js
+++ breweries.js
@@ -9,25 +9,25 @@ const breweries = [{
     id: 1,
     name: 'The Substance',
     style: 'IPA',
-    ABC: '6.6%',
+    ABV: '6.6%',
     logo: 'https://craftpeak-cooler-images.imgix.net/bissell-brothers/CE3A1042.jpg?auto=compress%2Cformat&fit=crop&h=600&ixlib=php-1.2.1&w=600&wpsize=tile_strict',
   }, {
     id: 2,
     name: 'Lux',
     style: 'Pale Ale',
-    ABC: '5.1%',
+    ABV: '5.1%',
     logo: 'https://craftpeak-cooler-images.imgix.net/bissell-brothers/Lux-square.jpg?auto=compress%2Cformat&fit=crop&h=600&ixlib=php-1.2.1&w=600&wpsize=tile_strict',
   }, {
     id: 3,
     name: 'Baby Genius',
     style: 'Blonde Ale',
-    ABC: '4%',
+    ABV: '4%',
     logo: 'https://craftpeak-cooler-images.imgix.net/bissell-brothers/BABYGENIUS.jpg?auto=compress%2Cformat&fit=crop&h=600&ixlib=php-1.2.1&w=600&wpsize=tile_strict',
   }, {
     id: 4,
     name: 'Dangol',
     style: 'Lager w/ Lime',
-    ABC: '5%',
+    ABV: '5%',
     logo: 'https://craftpeak-cooler-images.imgix.net/bissell-brothers/dangol_square.png?auto=compress%2Cformat&fit=crop&h=600&ixlib=php-1.2.1&w=600&wpsize=tile_strict',
   }]
 }, {
@@ -36,7 +36,31 @@ const breweries = [{
   location: 'Freeport',
   logo: 'https://mainebrewersguild.org/wp-content/uploads/2019/02/MaineBeerCo-Logo-2.jpg',
   website: 'http://www.mainebeercompany.com/',
-  beers:
+  beers: [{
+    id: 1,
+    name: 'Peeper',
+    style: 'Pale Ale',
+    ABV: '5.5%',
+    logo: 'https://images.squarespace-cdn.com/content/v1/58779d0203596ef4263164bb/1575840755344-QUV3QFJ1TJE2ISR8B9AF/ke17ZwdGBToddI8pDm48kNiEM88mrzHRsd1mQ3bxVct7gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z4YTzHvnKhyp6Da-NYroOW3ZGjoBKy3azqku80C789l0topjEaZcWjtmMYdCWL4dkGbxs35J-ZjFa9s1e3LsxrX8g4qcOj2k2AL08mW_Htcgg/Woods+Combo.png?format=500w',
+  }, {
+    id: 2,
+    name: 'Zoe',
+    style: 'Amber Ale',
+    ABV: '7.2%',
+    logo: 'https://images.squarespace-cdn.com/content/v1/58779d0203596ef4263164bb/1575840526082-76RJRPL7S8C811R14ZH9/ke17ZwdGBToddI8pDm48kNiEM88mrzHRsd1mQ3bxVct7gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z4YTzHvnKhyp6Da-NYroOW3ZGjoBKy3azqku80C789l0topjEaZcWjtmMYdCWL4dkGbxs35J-ZjFa9s1e3LsxrX8g4qcOj2k2AL08mW_Htcgg/Zoe+Combo.png?format=500w',
+  }, {
+    id: 3,
+    name: 'Mean Old Tom',
+    style: 'Stout',
+    ABV: '6.5%',
+    logo: 'https://images.squarespace-cdn.com/content/v1/58779d0203596ef4263164bb/1575840690853-WUPUH7EXUBAF58N289Z6/ke17ZwdGBToddI8pDm48kNiEM88mrzHRsd1mQ3bxVct7gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z4YTzHvnKhyp6Da-NYroOW3ZGjoBKy3azqku80C789l0topjEaZcWjtmMYdCWL4dkGbxs35J-ZjFa9s1e3LsxrX8g4qcOj2k2AL08mW_Htcgg/Mean+Old+Tom+Combo.png?format=500w',
+  }, {
+    id: 4,
+    name: 'Lunch',
+    style: 'IPA',
+    ABV: '7%',
+    logo: 'https://images.squarespace-cdn.com/content/v1/58779d0203596ef4263164bb/1575840287518-HLOVABN6K0QFQDL7FY7G/ke17ZwdGBToddI8pDm48kNiEM88mrzHRsd1mQ3bxVct7gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z4YTzHvnKhyp6Da-NYroOW3ZGjoBKy3azqku80C789l0topjEaZcWjtmMYdCWL4dkGbxs35J-ZjFa9s1e3LsxrX8g4qcOj2k2AL08mW_Htcgg/Lunch+Combo.png?format=500w',
+  }]
 }]
 
 module.exports = breweries
