735e29736bef4430418a3532d08e15a7ebe18618
Andy Schachter <andy.schachter@gmail.com>
2021-05-06 17:58:00 -0400

    created dummy object for sample breweries

---
+++ breweries.js
@@ -0,0 +1,42 @@
+/* eslint-disable max-len */
+const breweries = [{
+  id: 1,
+  name: 'Bissell Brothers',
+  location: 'Portland',
+  logo: 'https://mainebrewersguild.org/wp-content/uploads/2019/01/BBB-White-on-Black.png',
+  website: 'www.bissellbrothers.com',
+  beers: [{
+    id: 1,
+    name: 'The Substance',
+    style: 'IPA',
+    ABC: '6.6%',
+    logo: 'https://craftpeak-cooler-images.imgix.net/bissell-brothers/CE3A1042.jpg?auto=compress%2Cformat&fit=crop&h=600&ixlib=php-1.2.1&w=600&wpsize=tile_strict',
+  }, {
+    id: 2,
+    name: 'Lux',
+    style: 'Pale Ale',
+    ABC: '5.1%',
+    logo: 'https://craftpeak-cooler-images.imgix.net/bissell-brothers/Lux-square.jpg?auto=compress%2Cformat&fit=crop&h=600&ixlib=php-1.2.1&w=600&wpsize=tile_strict',
+  }, {
+    id: 3,
+    name: 'Baby Genius',
+    style: 'Blonde Ale',
+    ABC: '4%',
+    logo: 'https://craftpeak-cooler-images.imgix.net/bissell-brothers/BABYGENIUS.jpg?auto=compress%2Cformat&fit=crop&h=600&ixlib=php-1.2.1&w=600&wpsize=tile_strict',
+  }, {
+    id: 4,
+    name: 'Dangol',
+    style: 'Lager w/ Lime',
+    ABC: '5%',
+    logo: 'https://craftpeak-cooler-images.imgix.net/bissell-brothers/dangol_square.png?auto=compress%2Cformat&fit=crop&h=600&ixlib=php-1.2.1&w=600&wpsize=tile_strict',
+  }]
+}, {
+  id: 2,
+  name: 'Maine Beer Company',
+  location: 'Freeport',
+  logo: 'https://mainebrewersguild.org/wp-content/uploads/2019/02/MaineBeerCo-Logo-2.jpg',
+  website: 'http://www.mainebeercompany.com/',
+  beers:
+}]
+
+module.exports = breweries
