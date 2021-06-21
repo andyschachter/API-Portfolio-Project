cb3cd666759c2cb40be28e8067c8f150054ad677
Andy Schachter <andy.schachter@gmail.com>
2021-06-21 00:11:49 -0400

    added some more data to initial data load migration

--- migrations/20210621040157-initial-data-load.js
+++ migrations/20210621040157-initial-data-load.js
@@ -27,8 +27,26 @@ module.exports = {
       style: 'IPA',
       abv: '6.6%',
       logo: 'https://craftpeak-cooler-images.imgix.net/bissell-brothers/CE3A1042.jpg?auto=compress%2Cformat&fit=crop&h=600&ixlib=php-1.2.1&w=600&wpsize=tile_strict',
-      breweryId: 1,
-    },
+      breweryId: 1
+    }, {
+      name: 'Lux',
+      style: 'Pale Ale',
+      abv: '5.1%',
+      logo: 'https://craftpeak-cooler-images.imgix.net/bissell-brothers/Lux-square.jpg?auto=compress%2Cformat&fit=crop&h=600&ixlib=php-1.2.1&w=600&wpsize=tile_strict',
+      breweryId: 1
+    }, {
+      name: 'Baby Genius',
+      style: 'Blonde Ale',
+      abv: '4%',
+      logo: 'https://craftpeak-cooler-images.imgix.net/bissell-brothers/BABYGENIUS.jpg?auto=compress%2Cformat&fit=crop&h=600&ixlib=php-1.2.1&w=600&wpsize=tile_strict',
+      breweryId: 1
+    }, {
+      name: 'Dangol',
+      style: 'Lager w/ Lime',
+      abv: '5%',
+      logo: 'https://craftpeak-cooler-images.imgix.net/bissell-brothers/dangol_square.png?auto=compress%2Cformat&fit=crop&h=600&ixlib=php-1.2.1&w=600&wpsize=tile_strict',
+      breweryId: 1
+    }
     ])
   },
 
