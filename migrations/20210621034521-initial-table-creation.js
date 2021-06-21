57756331b6724b93d4d9958cad89ac1264f997b1
Andy Schachter <andy.schachter@gmail.com>
2021-06-20 23:53:14 -0400

    added beers table to initial table creation migration

--- migrations/20210621034521-initial-table-creation.js
+++ migrations/20210621034521-initial-table-creation.js
@@ -22,14 +22,33 @@ module.exports = {
       },
       deletedAt: { type: Sequelize.DATE, },
     })
+
+    return queryInterface.createTable('beers', {
+      id: { type: Sequelize.INTEGER, autoIncrement: true, primaryKey: true },
+      name: { type: Sequelize.TEXT, allowNull: false },
+      style: { type: Sequelize.TEXT, allowNull: false },
+      abv: { type: Sequelize.STRING, allowNull: false },
+      logo: { type: Sequelize.TEXT, allowNull: false },
+      breweryId: { type: Sequelize.INTEGER, references: { model: 'breweries', key: 'id' } },
+      createdAt: { type: Sequelize.DATE, allowNull: false, defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'), },
+      updatedAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'),
+      },
+      deletedAt: { type: Sequelize.DATE, },
+    })
   },
 
-  down: async (queryInterface, Sequelize) => {
+  down: async (queryInterface) => {
     /**
      * Add reverting commands here.
      *
      * Example:
      * await queryInterface.dropTable('users');
      */
+    await queryInterface.dropTable('beers')
+
+    return queryInterface.dropTable('breweries')
   }
-};
+}
